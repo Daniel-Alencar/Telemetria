347e61c598baab3c8b46a934b2ee907b66eed843
Daniel-Alencar <danielalencar746@gmail.com>
2021-11-10 22:06:49 -0300

    Co-authored-by: GkingOficial <GkingOficial@users.noreply.github.com> Co-authored-by: alanvictor48 <alanvictor48@users.noreply.github.com>

--- ARMAZENAMENTO_BIBLIOTECA.ino
+++ ARMAZENAMENTO_BIBLIOTECA.ino
@@ -4,13 +4,16 @@
 #define CS_PIN 8
 File myFile;
 
-void settings() {
-  SD.begin(CS_PIN);
-  if(myFile) {
-    myFile = SD.open("data.txt", FILE_WRITE);
-    myFile.println("TEMPO, ALTITUDE, AltitudeFk, AY, Temperatura");
-    myFile.close();
+bool settingsSD() {
+  if(SD.begin()) {
+    if(myFile) {
+      myFile = SD.open("data.txt", FILE_WRITE);
+      myFile.println("TEMPO, ALTITUDE, AltitudeFk, AY, Temperatura");
+      myFile.close();
+      return true;
+    }
   }
+  return false;
 }
 
 void writeOnSD(float time, float altitude, float altitudeFk, float AY, float temperatura) {
@@ -18,9 +21,11 @@ void writeOnSD(float time, float altitude, float altitudeFk, float AY, float tem
   myFile = SD.open("data.txt", FILE_WRITE);
 
   if(myFile) {
-    str = String(time,2) + "," + String(altitude,2) + "," + String(altitudeFk,2) + "," + String(AY,2) + "\n";
+    str = String(time,2) + "," + String(altitude,2) + "," + String(altitudeFk,2) + "," + String(AY,2) + String(temperatura,2);
 
     myFile.println(str);
     myFile.close();
   }
 }
+
+
