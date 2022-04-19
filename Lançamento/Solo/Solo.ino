d0875c75fe963dff28c82b1c79ef6d91655ba1aa
Daniel-Alencar <danielalencar746@gmail.com>
2022-04-19 19:31:08 -0300

    Ajustes para a interface de telemetria

--- Lançamento/Solo/Solo.ino
+++ Lançamento/Solo/Solo.ino
@@ -1,8 +1,14 @@
 // Declaração de bibliotecas
 #include "libs/NRF24L01_BIBLIOTECA.h"
 
+// #define ON_BUTTON 4
+// bool button = false;
+bool notSent = false;
+
 void setup() {
   Serial.begin(9600);
+
+  // pinMode(ON_BUTTON, INPUT);
 
   // Inicializa a comunicação com o modulo de rádio
   longRangeSettings();
@@ -11,6 +17,16 @@ void setup() {
 }
 
 void loop() {
+  if(!notSent){
+    Serial.println("INICIADO!");
+    notSent = true;
+  }
+
+  // button = digitalRead(ON_BUTTON);
+  // if(button) {
+  //   sendMessage("Ativa!");
+  // }
+
   readMessage();
   Serial.println(message);
 }
